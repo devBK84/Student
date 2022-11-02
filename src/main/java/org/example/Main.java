29f815db8641647c35c5facf868e0ded523988b9
devBK84 <bk@kharashfeh.com>
2022-11-02 14:55:47 +0100

    Add Student class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Lege eine Klasse Student an und überlege dir drei Eigenschaften
+// Schreibe Methoden, um die Eigenschaften zu setzen und abzufragen
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+// Das ist eine Instanz mit dem namen Studentlist
+
+        Student student1 = new Student("Alf", 23, 1.98);
+
+//        student1.Age = 21;
+//        student1.Height = 1.98;
+//        student1.Name = "Alf";
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+
+        System.out.println(student1.age);
+        System.out.println(student1.height);
+        System.out.println(student1.name);
+
     }
+  // Verschiebe deine Student Klasse in ein model Package und passe die Sichtbarkeiten und Referenzen an
+    // Schreibe eine Methode toString welche alle Eigenschaften eines Studenten als String zurückgibt
+    //Schreibe für deinen Student Klasse einen Konstruktor und verwende ihn
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,18 @@
-package org.example;public class Student {
+package org.example;
+
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+
+public class Student {
+
+    String name;
+    int age;
+    double height;
+
+// Das ist eine Konstruktor
+    public Student(String name, int age, double height) {
+
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
 }
